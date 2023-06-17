f76c28f9f6e9ff7a0602ec4b90256cb0ad98c2cc
Garen <karmartir@gmail.com>
2023-06-17 00:26:56 -0400

    with css

---
+++ src/Logo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Logo = () => {
+    return (
+        <div>
+            <h4 className='title_3'>|---|0|---|X|---|X|---|</h4>
+            <h4 className='title_3'>|---|X|---|X|---|0|---|</h4>
+            <h4 className='title_3'>|---|X|---|0|---|0|---|</h4>
+            <br/>
+
+            <br/>
+            <h1 className='title'>The GAME </h1>
+            <h1 className='title_name'>--- "TIC-TAC-TOE" ---  </h1>
+        </div>
+    );
+};
+
+export default Logo;
